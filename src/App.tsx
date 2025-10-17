f2e81beb20ec5cfa4380efcfeda9c23f57f4a19f
Timofei Asinski <tasinski@dhcp-10-105-51-86.wireless.northwestern.private>
2025-10-16 20:49:57 -0500

    added group me functionality. Added axios library to perform https requests

--- src/App.tsx
+++ src/App.tsx
@@ -107,6 +107,7 @@ function App() {
               recipients: params.recipients
             });
           } else if (params.channel === 'groupme') {
+            console.log("sendingGroupme");
             success = await sendGroupMe({
               message: params.message,
               recipients: params.recipients
--- src/utilities/sendGroupMe.ts
+++ src/utilities/sendGroupMe.ts
@@ -1,3 +1,7 @@
+
+
+import axios from 'axios';
+
 export interface GroupMeRecipient {
   id: string;
   name: string;
@@ -8,12 +12,31 @@ export interface SendGroupMeParams {
   message: string;
   recipients: GroupMeRecipient[];
 }
-
+const accessToken = "VFPaf8IblVQ2Mu5Z445a2Q0pUH1azNu8eyI7dQTo"; //somehow we get this per RA user
+const groupID = "111036119"; //RA user has to input this
 export const sendGroupMe = async (params: SendGroupMeParams): Promise<boolean> => {
   try {
+    const randomString = Math.random().toString(36).substring(2, 15); //random guid
+    const messageData = {
+      message: {
+        source_guid: randomString,
+        text: params.message
+      }
+    };
+
     console.log('Would send to recipients:', params.recipients);
     console.log('Message:', params.message);
-
+    axios.post(`https://api.groupme.com/v3/groups/${groupID}/messages`, 
+      messageData,
+      {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        params: {
+          token: accessToken
+        } 
+      } 
+    ); 
     await new Promise((resolve) => setTimeout(resolve, 1000));
     return true;
   } catch (error) {
